78152e058d7a96944688a173cc1c1a812909acf1
jpvargues <j.p.b.vargues@gmail.com>
2026-01-14 17:18:45 +0000

    integration of gemini

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("GEMINI_API_KEY environment variable not found. Please ensure it's set in your environment or .env file.")
+client = genai.Client(api_key=api_key)
+
 def main():
-    print("Hello from aiagent!")
+    response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",)
+    print(response.text)
 
 
 if __name__ == "__main__":
