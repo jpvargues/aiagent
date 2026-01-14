7fb46877e9abeb16e5c6a51e117b4fbab308f150
jpvargues <j.p.b.vargues@gmail.com>
2026-01-14 18:19:47 +0000

    added verbose output

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
-import os
 import argparse
+import os
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if api_key is None:
-    raise RuntimeError("GEMINI_API_KEY environment variable not found. Please ensure it's set in your environment or .env file.")
-client = genai.Client(api_key=api_key)
 
 def main():
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
     args = parser.parse_args()
 
-    response = client.models.generate_content(
-    model='gemini-2.5-flash',
-    contents=args.user_prompt,)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-    if response.usage_metadata is None:
-        raise RuntimeError("API request failed: usage_metadata is None")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+    )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
     print(response.text)
 
 
